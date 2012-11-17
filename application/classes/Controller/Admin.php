fe8112487c67a3d1a974ab55342ed77694e91d48
David Power <david@karujahundu.com>
2012-11-17 21:27:30 +0000

    updating the database model

--- application/classes/Controller/Admin.php
+++ application/classes/Controller/Admin.php
@@ -5,14 +5,16 @@ defined('SYSPATH') or die('No direct script access.');
 class Controller_Admin extends Controller {
 
     public function action_index() {
-        $id = $this->request->param('id');
+        
         $temperature = $this->request->query('temperature');
         $humidity = $this->request->query('humidity');
 
         $head = '<title>ARDUINO LOGGER</title>';
         $body = 'Temperature: ' . $temperature . ' Humidity: ' . $humidity;
         
-        $data = Model_Webmodel::get_all_devices();
+        $service = new Model_Webmodel();
+        
+        $data = $service->getDevices();
         
         $this->response->body("<html><head>" . $head . "</head><body>" . $body . "</body></html>");
     }
--- application/classes/Model/webmodel.php
+++ application/classes/Model/webmodel.php
@@ -4,38 +4,13 @@ defined('SYSPATH') or die('No direct script access.');
 
 class Model_Webmodel extends ORM {
 
-    public function get_all_devices() {
+    public function getDevices() {
 
         $results = DB::select()
                 ->from('webservice_devices')
-                ->as_object()
                 ->execute();
-        return $results;
+        
+        return $results->as_array();
     }
-
-    public function all_categories() {
-
-        $results = DB::select()
-                ->from('post_category')
-                ->execute();
-        $array = array();
-
-        foreach ($results as $id => $category) {
-            $array[$category['id']] = $category['category'];
-        }
-
-        return $array;
-    }
-
-    public function get_type($id) {
-
-        $results = DB::select('type')
-                ->from('posts')
-                ->where('id', '=', $id)
-                ->execute()
-                ->current();
-        return $results;
-    }
-
 }
 ?>
