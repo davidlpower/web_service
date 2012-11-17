ea25ccea9ab8dbecf8c70f26a559e56e3207d3f7
David Power <david@karujahundu.com>
2012-11-17 18:48:45 +0000

    milestone 1

--- application/classes/Controller/Admin.php
+++ application/classes/Controller/Admin.php
@@ -1,10 +1,25 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Admin extends Controller {
 
-	public function action_index()
-	{
-		$this->response->body('hello, world!');
-	}
+    public function action_index() {
+        
+        $id = $this->request->param('id');
+        
+        $view = View::factory('index');
 
-} // End Welcome
+
+        $view->head = '<html><head></head>';
+        $view->body = '<body>The ID is: ' . $id . '</body>';
+        $view->foot = '</html>';
+
+        $index_page = $view->render();
+        // The view will have $places and $user variables
+        $this->request->response = $index_page;
+    }
+
+}
+
+// End Admin
