73efabdae89ffec9be9abb2485d155892a6aa3a4
David Power <david@karujahundu.com>
2012-11-18 01:56:41 +0000

    end of work commits

--- application/classes/Controller/Admin.php
+++ application/classes/Controller/Admin.php
@@ -5,19 +5,43 @@ defined('SYSPATH') or die('No direct script access.');
 class Controller_Admin extends Controller {
 
     public function action_index() {
-        
+
         $temperature = $this->request->query('temperature');
         $humidity = $this->request->query('humidity');
+        $device = $this->request->query('device');
 
         $head = '<title>ARDUINO LOGGER</title>';
-        $body = 'Temperature: ' . $temperature . ' Humidity: ' . $humidity;
+        $body = 'Device: ' . $device . '<br /> Temperature: ' . $temperature . '<br /> Humidity: ' . $humidity;
         
         $service = new Model_Webmodel();
-        // $service = new Model_Webmodel();
+
+        $result = $service->getDevice($device);
         
-        $data = $service->getDevices();
+        if ($result == TRUE)
+        {
+            echo 'Registered';
+            $service->save($temperature,$humidity);
+        }
+        else
+        {
+            echo 'Not Registered';
+        }
         
+        echo '<br /><br />';
         $this->response->body("<html><head>" . $head . "</head><body>" . $body . "</body></html>");
+    }
+
+    public function action_save() {
+
+        $temperature = $this->request->query('temperature');
+        $humidity = $this->request->query('humidity');
+        $device = $this->request->query('device');
+
+        $service = new Model_Webmodel();
+
+        $data = $service->getDevice('168CC2CDE1926121FDB78164D5B237C626E33121DC08B048C37B2BE5DFF4B8F1');
+
+        $this->response->body(1);
     }
 
 }
--- application/classes/Model/webmodel.php
+++ application/classes/Model/webmodel.php
@@ -2,15 +2,38 @@
 
 defined('SYSPATH') or die('No direct script access.');
 
-class Model_Webmodel extends Model_Database {
+class Model_Webmodel extends Model {
 
-    public function getDevices() {
+    public function getDevice($device_id) {
 
         $results = DB::select()
                 ->from('webservice_devices')
+                ->where('device_id', '=', $device_id)
+                ->and_where('allowed', '=', 1)
+                ->and_where('deleted', '=', 0)
                 ->execute();
         
-        return $results->as_array();
+        echo '<PRE>';
+        print_r($results->as_array());
+        echo '</PRE>';
+        
+        if($results) 
+        {
+            return TRUE;
+        }
+        else 
+        {
+            return FALSE;
+        }
     }
+
+    public static function save($temp,$humid) {
+
+        $results = DB::insert('webservice_temperature', array('date', 'temperature', 'humidity'))
+                ->values(array(DB::expr('NOW()'), $temp, $humid))
+                ->execute();
+    }
+
 }
+
 ?>
