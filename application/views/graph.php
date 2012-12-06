c766a01c9e4977d72b55389aebcfc57b43db0088
David Power <david@karujahundu.com>
2012-12-06 23:53:09 +0000

    add index page

--- application/views/graph.php
+++ application/views/graph.php
@@ -1,10 +1,19 @@
-<?php
-$data = null;
-foreach ($temp as $key => $value) {
-    $data[$value['Average_Temp']] = $value['date'];
-}
-?>
-<html>
-    <h3>This is where I want to display my graph</h3>
-    <img src="graph_bar.php?mydata=<?php echo urlencode(serialize($data)); ?>" />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <title>
+            Current Sitting Room Data
+        </title>
+    </head>
+    <body style="font-family: Arial;border: 0 none;">
+        <div id="container" style="width: 900px; margin: 0 auto;">
+            <div id="table_title"><h1>List of Graphs</h1></div>
+            <ul>
+                <li><a href="/graph/currentdata">Daily Breakdown</a></li>
+                <li><a href="/graph/currentdata">Weekly Breakdown</a></li>
+                <li><a href="/graph/currentdata">Monthly Breakdown</a></li>
+            </ul>
+        </div>
+    </body>
 </html>
