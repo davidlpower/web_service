6039b186ceb69b1d1b01d1d66c3a6b59c6fe0594
David Power <david@karujahundu.com>
2012-12-06 23:53:34 +0000

    fix index contorller

--- application/classes/Controller/Graph.php
+++ application/classes/Controller/Graph.php
@@ -6,32 +6,7 @@ class Controller_Graph extends Controller {
 
     public function action_index() {
 
-        $head = "<title>Arduino Temperature Logger</title>";
-
-        // Get data from database
-        $service = new Model_Webmodel();
-        $result = $service->getPercentageDifference();
-        $temp = $service->getBreakdown();
-
-        if ($result == 'TRUE')
-        {
-            $background = 'style="background: #EE6363;"';
-        }
-        elseif ($result == 'FALSE')
-        {
-            $background = 'style="background: #66CCCC;"';
-        }
-        else
-        {
-            $background = 'style="background: #FFFFF1;"';
-        }
-
-        $view = View::factory('graph')
-                ->set('head', $head)
-                ->set('background', $background)
-                ->set('barchart', $this->get_barchart($temp));
-
-
+        $view = View::factory('graph');
         echo $view->render();
     }
 
