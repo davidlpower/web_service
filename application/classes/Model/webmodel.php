6321078d90cc861cbe664bad6e151fa673918e24
David Power <david@karujahundu.com>
2012-12-10 21:19:45 +0000

    Update the record var view.

--- application/classes/Model/webmodel.php
+++ application/classes/Model/webmodel.php
@@ -8,7 +8,7 @@ class Model_Webmodel extends Model {
     public static $view2 = "view_day_breakdown";
     public static $view3 = "view_week_breakdown";
     public static $view4 = "view_month_breakdown";
-    public static $record = "view_week_breakdown";
+    public static $record = "view_record";
     
     public function getDevice($device_id) {
 
