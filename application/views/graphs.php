c9b5c0c8c5a4317023e3258dbd600c49becc3ea5
David Power <david@karujahundu.com>
2012-12-16 17:56:55 +0000

    jquery test

--- application/views/graphs.php
+++ application/views/graphs.php
@@ -31,9 +31,10 @@
 
 <script>
     $('#device').change(function() {
-        var device_id = this.val();
-        $('body a').each(function(data){
-            console.log(data);
-        })
+        $('body').each(function() {
+            $(this).children('a').each(function(index) {
+                $(this).after("<span></span>");
+            });
+        });
     });
 </script>
